bd03f9fc98401cb3bdb7491943c2b530b561b24e
Anas <anas@Cheeta.(none)>
2012-05-08 22:47:29 -0700

    initial

---
+++ src/test/TestGit.java
@@ -0,0 +1,16 @@
+package test;
+
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello world");
+		
+	}
+
+}
